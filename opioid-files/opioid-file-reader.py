d9523bfe71b69102e737d5cafd1a5a458bda2b5d
darrenfishell <darren.fishell@gmail.com>
2019-07-23 07:52:57 -0400

    Added census files and clean data files

--- opioid-files/opioid-file-reader.py
+++ opioid-files/opioid-file-reader.py
@@ -1,6 +1,30 @@
 import pandas as pd
 import numpy as np
+import os
+cwd=os.getcwd()
 
-df = pd.read_csv('/Volumes/Photos & Storage/Big Data Store/arcos_all.tsv.gz', delimiter='\t'
-                ,compression='gzip', header=0, sep=' ', quotechar='"'
-                ,encoding='utf-8', error_bad_lines=False)
+df = pd.read_csv('arcos-me-statewide-itemized.tsv', delimiter='\t', low_memory=False)
+
+df.head()
+
+townfix = {
+    'lisbon falls':'lisbon'
+    ,'bridgeton':'bridgton'
+    ,'cumberland center':'cumberland'
+    ,'grey':'gray'
+    ,'north windham':'windham'
+    ,'steep falls':'standish'
+    ,'seal harbor':'mount desert'
+    ,'east boothbay':'boothbay'
+    ,'no. vassalboro':'vassalboro'
+    ,'south paris':'paris'
+    ,'east waterboro':'waterboro'
+    ,'hollis center':'hollis'
+    ,'springvale':'sanford'
+}
+
+df['lower_city'] = df['BUYER_CITY'].lower()
+
+df.lower(BUYER_CITY).replace(townfix,value=None,inplace=True)
+
+df.to_csv(cwd+'/arcos-me-statewide-cleaned-towns.csv')
