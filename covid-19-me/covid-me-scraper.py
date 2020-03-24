66ba053c06e04f9979cef7d225b57a46150b3934
darrenfishell <darren.fishell@gmail.com>
2020-03-24 13:01:37 -0400

    New modules

--- covid-19-me/covid-me-scraper.py
+++ covid-19-me/covid-me-scraper.py
@@ -8,9 +8,9 @@ import json
 
 #Dictionary of table match (key], then (header_row, column count]
 shape_dict = {'Testing Data':{'header_row':2, 'filename':'case_summary'}
-            ,'Confirmed and Recovered Case Counts by County':{'header_row':1, 'filename':'cases_by_county'}
-            ,'Confirmed and Presumptive Cases by Age':{'header_row':1,'filename':'cases_by_age'}
-            ,'Confirmed and Presumptive Cases by Sex':{'header_row':1,'filename':'cases_by_sex'}}
+            ,'Confirmed and Recovered Case Counts by County':{'header_row':2, 'filename':'cases_by_county'}
+            ,'Confirmed and Presumptive Cases by Age':{'header_row':2,'filename':'cases_by_age'}
+            ,'Confirmed and Presumptive Cases by Sex':{'header_row':2,'filename':'cases_by_sex'}}
 
 matches = list(shape_dict)[0:]
 
@@ -22,15 +22,6 @@ column_list = {matches[0]: ['Confirmed Cases1','Negative Tests2']
 
 cols = list(column_list.values())
 
-#Pull in summary update time
-try:
-    update_time = pd.read_html('https://www.maine.gov/dhhs/mecdc/infectious-disease/epi/airborne/coronavirus.shtml',
-                     match=matches[0],header=None)[0].columns[0][1].replace('Updated: ', '').replace(' at ',' ').strip()
-except:
-    #Print error and exit if this fails
-    print('Update time load failed.')
-    exit(1)
-
 i=0
 y=0
 for x in matches:
@@ -38,6 +29,10 @@ for x in matches:
         #Finds tables in order of dictionary match, sets header row, specifies columns
         df = pd.read_html('https://www.maine.gov/dhhs/mecdc/infectious-disease/epi/airborne/coronavirus.shtml',
                           match=matches[i], header=list(shape_dict.values())[i]['header_row'])[0][cols[i]]
+
+        #Update time could now unique to each table -- March 22
+        update_time = pd.read_html('https://www.maine.gov/dhhs/mecdc/infectious-disease/epi/airborne/coronavirus.shtml',
+                                   match=matches[i], header=None)[0].columns[0][1].replace('Updated: ', '').replace(' at ', ' ').strip()
     except:
         #log failures
         y+=1
@@ -66,15 +61,14 @@ for x in matches:
 
         # Declare target sheet
         wks = sh.worksheet('index', i)
-
         dfg = wks.get_as_df()
         # print(df)
         # print(dfg)
 
         # Get target sheet as dataframe, combine with new load and eliminate dupes
-        df = pd.concat([df, dfg]).drop_duplicates()
+        df = pd.concat([df, dfg],sort=False).drop_duplicates()
 
-        # Truncate table and load modified dataframe
+        #Truncate table and load modified dataframe
         wks.clear()
         wks.rows = df.shape[0]
         wks.set_dataframe(df, start='A1', nan='')
