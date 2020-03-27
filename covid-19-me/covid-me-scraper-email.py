d49a0ca10da656e71ff67b4cbd80e2ded38cfe49
darrenfishell <darren.fishell@gmail.com>
2020-03-27 16:49:33 -0400

    Update script to add NYT state-level figures

--- covid-19-me/covid-me-scraper-email.py
+++ covid-19-me/covid-me-scraper-email.py
@@ -9,14 +9,14 @@ import json
 
 import config
 
-email_address = config.email_name
-pw = config.email_pass
+email_address = os.environ['EMAIL_NAME']
+pw = os.environ['EMAIL_PW']
 
 gc = pygsheets.authorize(service_file='covid-gcreds.json')
 
 # Dictionary of table match (key], then (header_row, column count]
 shape_dict = {'Testing Data': {'header_row': 2, 'filename': 'case_summary'}
-    , 'Confirmed and Recovered Case Counts by County': {'header_row': 2, 'filename': 'cases_by_county'}
+    , 'COVID-19 Case Counts by County': {'header_row': 2, 'filename': 'cases_by_county'}
     , 'Confirmed Cases by Age': {'header_row': 2, 'filename': 'cases_by_age'}
     , 'Confirmed Cases by Sex': {'header_row': 2, 'filename': 'cases_by_sex'}}
 
@@ -24,7 +24,7 @@ matches = list(shape_dict)[0:]
 
 # Manual column mapping for each table -- new columns will show null values for old records
 column_list = {matches[0]: ['Confirmed Cases1', 'Negative Tests2']
-    , matches[1]: ['County', 'Confirmed', 'Recovered']
+    , matches[1]: ['County', 'Confirmed', 'Recovered', 'Deaths']
     , matches[2]: ['Age Range', 'Count']
     , matches[3]: ['Sex', 'Count']}
 
@@ -39,16 +39,18 @@ for x in matches:
                           match=matches[i], header=list(shape_dict.values())[i]['header_row'])[0][cols[i]]
 
         # Update time could now unique to each table -- March 22
-        update_time = pd.to_datetime(pd.read_html('https://www.maine.gov/dhhs/mecdc/infectious-disease/epi/airborne/coronavirus.shtml',
-                                   match=matches[i], header=None)[0].columns[0][1].replace('Updated: ', '').replace(' at ', ' ').strip())
+        update_time = pd.to_datetime(
+            pd.read_html('https://www.maine.gov/dhhs/mecdc/infectious-disease/epi/airborne/coronavirus.shtml',
+                         match=matches[i], header=None)[0].columns[0][1].replace('Updated: ', '').replace(' at ',
+                                                                                                          ' ').strip())
     except:
-        #log failures
-        y+=1
-        print(str(list(shape_dict.values())[i]['filename'])+' failed to load or changed structures.')
+        # log failures
+        y += 1
+        print(str(list(shape_dict.values())[i]['filename']) + ' failed to load or changed structures.')
         # advance main iterator
-        i+=1
+        i += 1
     else:
-        #DETERMINE IF FILE IS NEW, IF SO EMAIL ALERT#
+        # DETERMINE IF FILE IS NEW, IF SO EMAIL ALERT#
         # Declare target sheet and get data
         sh = gc.open('covid-19-maine')
         wks = sh.worksheet('index', i)
@@ -59,20 +61,21 @@ for x in matches:
 
         if update_time > last_update_time:
 
-            print('CDC '+str(list(shape_dict.values())[i]['filename'])+str(update_time)+'. Prior update was at: '+str(last_update_time))
+            print('CDC ' + str(list(shape_dict.values())[i]['filename']) + str(
+                update_time) + '. Prior update was at: ' + str(last_update_time))
 
-            with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+            with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
                 smtp.ehlo()
                 smtp.starttls()
                 smtp.ehlo()
 
-                smtp.login(email_address,pw)
+                smtp.login(email_address, pw)
 
                 subject = f'MAINE CDC TABLE {filename} UPDATED AS OF {update_time}'
                 body = f'The prior update of {filename} was at {last_update_time} \n\n Data just refreshed at {update_time}'
                 msg = f'Subject: {subject}\n\n{body}'
 
-                smtp.sendmail(email_address,email_address,msg)
+                smtp.sendmail(email_address, email_address, msg)
 
             # Join county data table to county population records
             if matches[i] == matches[1]:
@@ -82,7 +85,7 @@ for x in matches:
                 df = pd.merge(df, results, left_on='County', right_on='county', how='left')
                 df.drop(['county'], axis=1, inplace=True)
 
-            #TRANSFORM DATAFRAME FOR LOAD#
+            # TRANSFORM DATAFRAME FOR LOAD#
             df.fillna('', inplace=True)
             df['timestamp'] = update_time
 
@@ -101,8 +104,20 @@ for x in matches:
                 df.to_csv(w, index=False)
 
         else:
-            print('CDC '+str(list(shape_dict.values())[i]['filename'])+' not updated. Last updated: '+str(last_update_time)+'. Current CDC timestamp: '+str(update_time))
-        i+=1
+            print('CDC ' + str(list(shape_dict.values())[i]['filename']) + ' not updated. Last updated: ' + str(
+                last_update_time) + '. Current CDC timestamp: ' + str(update_time))
+        i += 1
 
-print(str(i)+' files processed successfully.')
-print(str(y)+' files failed.')
+print(str(i) + ' files processed successfully.')
+print(str(y) + ' files failed.')
+
+##ADD NYT STATES FILE##
+df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
+wks = sh.worksheet('index', 4)
+wks.clear()
+wks.rows = df.shape[0]
+wks.set_dataframe(df, start='A1', nan='')
+
+##WRITE NYT file to data.world##
+with dw.open_remote_file('darrenfishell/covid-19-me', 'nyt_states.csv') as w:
+    df.to_csv(w, index=False)
