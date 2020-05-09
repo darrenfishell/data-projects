cb69af548f6e07b26f1906ec7345b2d88e2581fc
darrenfishell <darren.fishell@gmail.com>
2020-05-09 15:03:38 -0400

    Maine elections map and code updates

--- maine-campaign-finance/maine-ethics-scraper.py
+++ maine-campaign-finance/maine-ethics-scraper.py
@@ -1,7 +1,7 @@
-import numpy as np
 import pandas as pd
 import requests
 import os
+import config
 import pygsheets
 import http.client
 import datadotworld as dw
@@ -9,127 +9,149 @@ import json
 from io import StringIO
 from pandas.io.json import json_normalize
 
+event=[]
+context=[]
+lambda_handler(event,context)
 
-years = ['2020']
+def lambda_handler(event, context):
+    # Initialize session
+    s = requests.Session()
+    cookies = requests.cookies.RequestsCookieJar()
 
-class MaineEthics():
+    # URL and session variables
+    url_base = config.url_base
+    headers = config.headers
+    year = config.year
 
-    def get_lookup(self,year):
-        #Initialize session
-        s = requests.Session()
-        cookies = requests.cookies.RequestsCookieJar()
+    print(year)
 
-        url = 'https://mainecampaignfinance.com/api///Organization/SearchCandidates'
+    # Execute queries
+    try:
+        length = get_cands(year, headers)
+        print(f'Wrote records for {length} candidates.')
+    except:
+        print('Failed to write candidate lookup.')
 
-        self.year = year.split(',')
+    # Get candidate contributions
+    try:
+        get_trans(year, headers)
+        print('Contributions loaded')
+    except:
+        print('Failed to write some file')
 
-        cand_q = {"ElectionYear":,
-                  "pageNumber": 1,
-                  "pageSize": 2147483647}
-
-        r = s.post(url, data=json.dumps(cand_q), headers=headers).json()
-        df = json_normalize(r)
-
-        with dw.open_remote_file('darrenfishell/2020-maine-state-campaign-finance', 'candidate_lookup.csv') as w:
-            df.to_csv(w, index=False)
-
-
-def get_spending():
-
-
+    try:
+        write_to_gsheet()
+        print('Wrote records to GSheet')
+    except:
+        print("Write to GSheet failed")
 
 
+# GET CANDS / TRANSACTION FUNCTIONS
+def get_cands(year, headers):
+    # GET CANDIDATES LOOKUP#
+    url = 'https://mainecampaignfinance.com/api///Organization/SearchCandidates'
 
-## Set Variables ##
-url = 'https://mainecampaignfinance.com/api///Search/TransactionSearchInformationExpExportToCSV'
-url_base = 'https://mainecampaignfinance.com'
+    data = {"ElectionYear": year,
+            "pageNumber": 1,
+            "pageSize": 2147483647}
 
-# Can search multiple, comma-separated
-election_year = '2020,2019'
+    s = requests.Session()
+    cookies = requests.cookies.RequestsCookieJar()
 
-# Parameters for looping through search
-committee_types = {'candidate': '01'
-    , 'bqc': '02'
-    , 'pac': '03'
-    , 'party-committee': '09'}
+    r = s.post(url, data=json.dumps(data), headers=headers).json()
 
-transaction_types = {'contributions': 'CON'
-    , 'expenditures': 'EXP'
-    , 'independent_expenditures': 'IE'
-    , 'ballot_questions': 'All'
-                     }
+    df = json_normalize(r)
 
-trans_types = list(transaction_types.values())
-filenames = list(transaction_types.keys())
-comm_types = list(committee_types.values())
+    length = len(df)
 
-# Data dictionary for query
-data = {"ElectionYear": election_year
-    , "pageNumber": '1'
-    , "pageSize": '2147483647'  # Sets max responses from page (defaults to 10)
-    , "ValidationRequired": '0'}
-
-# POST headers
-headers = {'Host': 'mainecampaignfinance.com'
-    , 'Origin': url_base
-    , 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:69.0) Gecko/20100101 Firefox/69.0'
-    , 'Accept': 'application/octet-stream'
-    , 'Accept-Language': 'en-US,en;q=0.5'
-    , 'Accept-Encoding': 'gzip, deflate, br'
-    , 'Referer': 'https://mainecampaignfinance.com/'
-    , 'Content-Type': 'application/json;charset=utf-8'
-    , 'Connection': 'keep-alive'
-    , 'TE': 'Trailers'
-    , 'Pragma': 'no-cache'
-    , 'Cache-Control': 'no-cache'}
-
-# GET SPENDING REPORTS#
-# Set index
-x = 0
-for b in trans_types:
-
-    data.update({"TransactionType": trans_types[x]})
-
-    # Declare/reset df and reset index
-    dfs = []
-    i = 0
-
-    for n in comm_types:
-        data.update({"CommitteeType": comm_types[i]})
-
-        r = s.post(url, data=json.dumps(data), headers=headers)
-        df = pd.read_csv(StringIO(r.content.decode('utf-8')))
-        df['comm_type'] = comm_types[i]
-        dfs.append(df)
-        i += 1
-
-    # Combine dfs
-    df = pd.concat(dfs, sort=False, ignore_index=True).drop_duplicates()
-
-    # count = dw.query('darrenfishell/2020-maine-state-campaign-finance', 'SELECT COUNT(*) FROM '+filenames[x]).dataframe['count'][0]
-    # if count < len(df):
-    print('Writing file ' + filenames[x] + '.csv to data.world, at ' + str(len(df)) + ' records.')
-    with dw.open_remote_file('darrenfishell/2020-maine-state-campaign-finance', filenames[x] + '.csv') as w:
+    with dw.open_remote_file('darrenfishell/2020-maine-state-campaign-finance', 'candidate_lookup.csv') as w:
         df.to_csv(w, index=False)
 
-    # Advance to next transaction type
-    x += 1
+    return length
 
 
+def get_trans(year, headers):
+    url = 'https://mainecampaignfinance.com/api///Search/TransactionSearchInformationExpExportToCSV'
 
-# GET QUESTION ONE COMMITTEES#
-url = 'https://mainecampaignfinance.com/api///Organization/SearchAssociatedBallotQuestionCommittees'
+    s = requests.Session()
+    cookies = requests.cookies.RequestsCookieJar()
 
-vax_dict = {"BallotQuestionTypeId": "322"
-    , "ElectionYear": "2020"
-    , "pageNumber": "1"
-    , "pageSize": '10'
-    , "sortDir": "desc"
-    , "sortedBy": ""}
+    # Parameters for looping through search
+    committee_types = {'candidate': '01'
+        , 'bqc': '02'
+        , 'pac': '03'
+        , 'party-committee': '09'}
 
-r = s.post(url, data=json.dumps(vax_dict), headers=headers).json()
+    transaction_types = {'contributions': 'CON'
+        , 'expenditures': 'EXP'
+        , 'independent_expenditures': 'IE'}
 
-df = json_normalize(r)
+    trans_types = list(transaction_types.values())
+    filenames = list(transaction_types.keys())
+    comm_types = list(committee_types.values())
 
-with dw.open_remote_file('darrenfishell/2020-maine-state-campaign-finance', 'question-one-committees.csv') as w:
-    df.to_csv(w, index=False)
+    # Data dictionary for query
+    data = {"ElectionYear": year
+        , "pageNumber": '1'
+        , "pageSize": '2147483647'  # Sets max responses from page (defaults to 10)
+        , "ValidationRequired": '0'}
+
+    for idx, b in enumerate(transaction_types):
+
+        data.update({"TransactionType": trans_types[idx]})
+
+        dfs = []
+
+        # Reset index
+
+        for i, n in enumerate(comm_types):
+
+            data.update({"CommitteeType": comm_types[i]})
+
+            try:
+                r = s.post(url, data=json.dumps(data), headers=headers)
+                df = pd.read_csv(StringIO(r.content.decode('utf-8')))
+                dfs.append(df)
+            except:
+                continue
+
+        # COMBINE dfs
+        df = pd.concat(dfs, sort=False, ignore_index=True).drop_duplicates()
+
+        length = len(df)
+
+        file = filenames[idx]
+
+        # count = dw.query('darrenfishell/2020-maine-state-campaign-finance', 'SELECT COUNT(*) FROM '+filenames[idx]).dataframe['count'][0]
+        # if count < len(df):
+        with dw.open_remote_file('darrenfishell/2020-maine-state-campaign-finance', file + '.csv') as w:
+            df.to_csv(w, index=False)
+        print(f'Wrote file {file}.csv to data.world, at {length} records.')
+        # else:
+        #     print(f'No updates to {file}.')
+
+
+def write_to_gsheet():
+    # Write contribution query to GSheets
+    gc = pygsheets.authorize(service_file='gcreds.json')
+    conn = http.client.HTTPSConnection("api.data.world")
+
+    headers = {'authorization': "Bearer " + os.environ['DW_AUTH_TOKEN']}
+
+    queryid = 'a65bf908-26ba-4f11-b413-a57bd8b3a9f5'
+    project = '2020-maine-state-campaign-finance'
+    gsh_idx = 0
+    sheet = 'maine-state-campaign-finance-2020'
+
+    # Retrieve query
+    conn.request("GET", "/v0/queries/" + queryid, headers=headers)
+    data = conn.getresponse().read()
+    # Execute Query
+    results = dw.query('darrenfishell/' + project, json.loads(data)['body']).dataframe
+
+    # Prepare to load into Google Sheets
+    sh = gc.open(sheet)
+    wks = sh.worksheet('index', gsh_idx)
+    wks.clear()
+    wks.rows = results.shape[0]
+    wks.set_dataframe(results, start='A1', nan='')
