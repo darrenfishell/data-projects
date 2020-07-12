a5d3b78c23a8628c85b3e74c528bafedc34e962f
darrenfishell <darren.fishell@gmail.com>
2020-07-12 13:40:50 -0400

    Add mobility source and preliminary viz

--- covid-19-me/covid-csv-scraper.py
+++ covid-19-me/covid-csv-scraper.py
@@ -33,9 +33,13 @@ def write_to_gsheet(df, tab):
     wks = sh.worksheet('title', tab)
     dfg = wks.get_as_df()
 
-    if tab == 'nyt_states':
+    if tab == 'nyt_states' or tab == 'descartes_mobility':
 
-        last_update = pd.to_datetime(dfg['date']).max()
+        try:
+            last_update = pd.to_datetime(dfg['date']).max()
+        except:
+            last_update = pd.to_datetime('1901-01-01')
+
         new_file_dt = pd.to_datetime(df['date']).max()
 
         if new_file_dt > last_update:
@@ -44,10 +48,10 @@ def write_to_gsheet(df, tab):
             wks.clear()
             wks.rows = df.shape[0]
             wks.set_dataframe(df, start='A1', nan='')
-            print('NYT data refreshed')
+            print(f'{tab} refreshed')
 
         else:
-            print(f'NYT data not updated. Current through {last_update}')
+            print(f'{tab} data not updated. Current through {last_update}')
 
     else:
         last_update = pd.to_datetime(dfg['timestamp']).max().date()
@@ -77,15 +81,23 @@ sh = gc.open('covid-19-maine')
 county_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRPtRRaID4XRBSnrzGomnTtUUkq5qsq5zj8fGpg5xse8ytsyFUVqAKKypYybVpsU5cHgIbY3BOiynOC/pub?gid=0&single=true&output=csv'
 age_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRPtRRaID4XRBSnrzGomnTtUUkq5qsq5zj8fGpg5xse8ytsyFUVqAKKypYybVpsU5cHgIbY3BOiynOC/pub?gid=574023130&single=true&output=csv'
 nyt_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv'
-
+descartes_url = 'https://raw.githubusercontent.com/descarteslabs/DL-COVID-19/master/DL-us-mobility-daterow.csv'
 
 county = get_county()
 age = get_age()
 nyt_file = pd.read_csv(nyt_url, parse_dates=['date'])
 
+descartes_maine = pd.read_csv(descartes_url, parse_dates=['date'])
+
+print(descartes_maine)
+print(descartes_maine.columns)
+
+descartes_maine = descartes_maine.loc[(descartes_maine['admin1'] == 'Maine') & (descartes_maine['admin2'].notna())]
+
 df_to_gsheet = {'cases_by_county': county
     , 'cases_by_age': age
-    , 'nyt_states': nyt_file}
+    , 'nyt_states': nyt_file
+    , 'descartes_mobility': descartes_maine}
 
 for tab, df in df_to_gsheet.items():
 
